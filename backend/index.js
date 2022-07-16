0a683078ed720e6b4a3a8d99ba2a255a0cb5f38d
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-07-16 17:54:30 +0530

    changed theme

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
 const express = require("express"); 
 const path = require("path")
-
 const app = express();
 
-app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 const MainRouter = require("../backend/routes/main")
 const CompRouter = require("../backend/routes/components")
 app.use("/", MainRouter)
 app.use("/components", CompRouter)
 
-app.use(express.static(path.join(__dirname)));
-app.use(express.static('../frontend'));
+app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend/style/src')));
 
-app.listen(process.env.PORT, ()=> console.log("App Available now"));
+app.set('views', 'frontend/views');
+
+app.listen(3000, ()=> console.log("App Available now"));
