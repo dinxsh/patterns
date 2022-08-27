38f95437204c4c0e1e412c52a9fced677542d81d
blaze-pvt <talwadkerdinesh@gmail.com>
2022-08-28 00:33:08 +0530

    removed unused dependencies

--- backend/routes/components.js
+++ backend/routes/components.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const router = express.Router()
 
-// components router to be added in v2
+router.get("/allcomp", (req,res)=>{
+    res.render("../frontend/views/allcomp.html")
+})
+
+router.get("/button", (req,res)=>{
+    res.render("../frontend/view/button.html")
+})
+
+router.get("/accordion", (req,res)=>{
+    res.render("../frontend/view/accordion.html")
+})
+
+router.get("/carousel", (req,res)=>{
+    res.render("../frontend/view/carousel.html")
+})
+
+router.get("/form", (req,res)=>{
+    res.render("../frontend/view/form.html")
+})
 
 module.exports = router
