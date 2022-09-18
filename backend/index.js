5f21e709306e61c801ac87eb1718009a8ff9c239
dinxsh <talwadkerdinesh@gmail.com>
2022-09-18 20:15:34 +0530

    bug fixes

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,14 @@ const app = express();
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use(express.static(path.join(__dirname, "../frontend/views")));
+app.use(express.static(path.join(__dirname, "../frontend/")));
+app.use(express.static(path.join(__dirname, "../frontend/comp")));
 
 const MainRouter = require("../backend/routes/main")
 const CompRouter = require("../backend/routes/components")
+const ApiRouter = require("../backend/routes/api")
 app.use("/", MainRouter)
 app.use("/components", CompRouter)
+app.use("/api", ApiRouter)
 
-app.listen(5000, ()=> console.log("App Available now"));
+app.listen(process.env.PORT, ()=> console.log("App Available now"));
